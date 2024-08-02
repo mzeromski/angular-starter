494ff0d84076b6a3833014fb4c145c90d6b1fbdf
mzeromski <mznet1@gmail.com>
2024-08-02 23:30:07 +0200

    integrate eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,37 +3,54 @@ const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-const prettier = require('prettier-eslint');
-const eslintPluginPrettier = require('eslint-plugin-prettier');
 
 module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
+      // @ts-ignore
+      eslintPluginPrettierRecommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+  
     ],
     processor: angular.processInlineTemplates,
     rules: {
       'prettier/prettier': ['error'],
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+      "@typescript-eslint/no-non-null-assertion": "off",
+
+"@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/prefer-as-const": "error",
+        "@typescript-eslint/typedef": [
+          "error",
+          {
+            "arrowParameter": false,
+            "variableDeclaration": true,
+            "memberVariableDeclaration": true,
+            "arrayDestructuring": false,
+            "objectDestructuring": false
+          }
+        ],
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            "type": "attribute",
+            "prefix": "app",
+            "style": "camelCase"
+          }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            "type": "element",
+            "prefix": "app",
+            "style": "kebab-case"
+          }
+        ]
     },
   },
   {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'as2';
+  title: string = 'angular-starter';
 }
