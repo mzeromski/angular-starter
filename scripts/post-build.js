310262a547cfdeed5fe25f4bb3b4fa3b70affb02
mzeromski <mznet1@gmail.com>
2024-11-05 18:45:53 +0100

    fix build

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,9 +1,1 @@
-var fs = require('fs');
-
-var oldPath = 'dist/angular-starter/3rdpartylicenses.txt';
-var newPath = 'dist/angular-starter/browser/3rdpartylicenses.txt';
-
-fs.rename(oldPath, newPath, function (err) {
-  if (err) throw err;
-  console.log('Successfully moved 3rdpartylicenses.txt');
-});
+console.log('Just console log from post-build.js');
